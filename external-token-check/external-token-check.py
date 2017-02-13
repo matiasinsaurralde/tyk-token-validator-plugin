13a85df97d86ec4e19b368877d9b9d75ef90ef70
Matias Insaurralde <matias@insaurral.de>
2017-02-13 22:51:48 +0200

    Update middleware syntax, add error handling

--- external-token-check/external-token-check.py
+++ external-token-check/external-token-check.py
@@ -4,19 +4,30 @@ from gateway import TykGateway as tyk
 import json, requests
 
 @Hook
-def ExternalTokenValidatorMiddleware(request, session, spec):
+def ExternalTokenValidatorMiddleware(request, session, metadata, spec):
 
     token = request.get_header('Authorization')
 
     if token is None:
         return request, session
 
-    targetUrl = 'https:/auth.sipsynergy.co.uk/oauth/token'
+    targetUrl = 'https://auth.sipsynergy.co.uk/oauth/token'
 
-    r = requests.post(targetUrl, headers={'Authorization': token})
+    try:
+        r = requests.post(targetUrl, headers={'Authorization': token})
 
-    if r.status_code > 299
-        request.object.return_overrides.response_code = 401
-        request.object.return_overrides.response_error = 'Not authorized by Auth Server'
+        if r.status_code > 299:
+            print("The authentication failed")
+            request.object.return_overrides.response_code = 401
+            request.object.return_overrides.response_error = 'Not authorized by Auth Server'
+            return request, session, metadata
+    except Exception as e:
+        print("An error occured:", e)
+        return request, session, metadata
 
-    return request, session
+    print("The authentication was successful!")
+    session.rate = 1000.0
+    session.per = 1.0
+    metadata["token"] = "47a0c79c427728b3df4af62b9228c8ae"
+
+    return request, session, metadata
